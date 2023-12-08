2ce97d1b7a882fc8d74366ecc28f45d965fc8ae3
Edmondbrn <edmond.berne6@gmail.com>
2023-12-08 14:34:09 +0100

    Graph préliminaire

--- Script.R
+++ Script.R
@@ -25,7 +25,7 @@ table(data$smoking_status)
 # 67 fumeur actuel, 64 anciens fumeurs et 66 never
 
 table(data$case)
-# 101 Sain et 96 cancéreux
+# 101 Sains et 96 cancéreux
 
 table(data$case, data$gender)
 table(data$case, data$smoking_status)
@@ -36,11 +36,37 @@ qqnorm(data$age.recr)
 shapiro.test(data$age.recr)
 # répartition normale pas très claire
 
-hist(data$Albumin.adduct.of.Nacetylcysteine)
-hist(data$Albumin.adduct.of.CysGly)
+# Crée un vecteur pour les moyennes et comparaison en fonction des statut tabagique
+moyennes_par_etat = tapply(data$Albumin.adduct.of.Nacetylcysteine, data$smoking_status, mean)
+# Ecart type
+ecart_types_par_etat <- tapply(data$Albumin.adduct.of.Nacetylcysteine, data$smoking_status, sd)
+# barplot 
+bp = barplot(height = moyennes_par_etat, col = "skyblue", main = "Nacetylcystéine", ylim = c(0,5) )
+# Barre d'erreur sd
+arrows(bp, moyennes_par_etat, bp, moyennes_par_etat + ecart_types_par_etat, angle = 90, code = 3, length = 0.1, col = "black")
 
 
+moyennes_par_etat = tapply(data$Albumin.adduct.of.CysGly, data$smoking_status, mean)
+barplot(height = moyennes_par_etat, col = "skyblue", main = "CysGly" )
 
+moyennes_par_etat = tapply(data$Albumin.adduct.of.sulfonic.acid, data$smoking_status, mean)
+barplot(height = moyennes_par_etat, col = "skyblue", main = "Sulfonic" )
+
+moyennes_par_etat = tapply(data$Albumin.unadducted, data$smoking_status, mean)
+barplot(height = moyennes_par_etat, col = "skyblue", main = "Non lié" )
+
+kruskal.test(data$Albumin.adduct.of.Nacetylcysteine, data$smoking_status)
+dunn.test(data$Albumin.adduct.of.Nacetylcysteine, data$smoking_status)
+
+# Calcul des moyennes et écart-types par état
+moyennes_par_etat <- tapply(df$valeur, df$etat, mean)
+
+
+# Création du barplot
+bp <- barplot(moyennes_par_etat, col = "skyblue", main = "Moyennes par État", xlab = "État", ylab = "Moyenne")
+
+# Ajout des barres d'erreur (écart type)
+arrows(bp, moyennes_par_etat, bp, moyennes_par_etat + ecart_types_par_etat, angle = 90, code = 3, length = 0.1, col = "red")
 
 
 
