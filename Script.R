d90eb2b1db77f3d0c120f77e8fa6d5a2159adb15
Edmondbrn <edmond.berne6@gmail.com>
2023-12-23 17:12:07 +0100

    stat inférentirlle

--- Script.R
+++ Script.R
@@ -36,12 +36,7 @@ table(data$case)
 
 table(data$case, data$gender)
 table(data$case, data$smoking_status)
-chisq.test(table(data$case, data$smoking_status))
-#p-value = 3.058e-07 donc on rejette H0 et accepte H1 = différence de proportion (current a beaucoup de cancer et never pas beacoup)
 
-table(data$case,data$centre)
-chisq.test(table(data$case, data$centre))
-#p-value = 0.5459 soit <0,05 donc pas de différences significatives entre les centres)
 
 hist(data$age.recr)
 qqnorm(data$age.recr)
@@ -49,7 +44,7 @@ shapiro.test(data$age.recr)
 # répartition normale pas très claire
 
 # Crée un vecteur pour les moyennes et comparaison en fonction des statut tabagique
-moyennes_par_etat = tapply(data$Albumin.adduct.of.Nacetylcysteine, data$smoking_status, mean)
+(moyennes_par_etat = tapply(data$Albumin.adduct.of.Nacetylcysteine, data$smoking_status, mean))
 # Ecart type
 ecart_types_par_etat <- tapply(data$Albumin.adduct.of.Nacetylcysteine, data$smoking_status, sd)
 # barplot 
@@ -58,13 +53,13 @@ bp = barplot(height = moyennes_par_etat, col = "skyblue", main = "Nacetylcystéi
 arrows(bp, moyennes_par_etat, bp, moyennes_par_etat + ecart_types_par_etat, angle = 90, code = 3, length = 0.1, col = "black")
 
 
-moyennes_par_etat = tapply(data$Albumin.adduct.of.CysGly, data$smoking_status, mean)
+(moyennes_par_etat = tapply(data$Albumin.adduct.of.CysGly, data$smoking_status, mean))
 barplot(height = moyennes_par_etat, col = "skyblue", main = "CysGly" )
 
-moyennes_par_etat = tapply(data$Albumin.adduct.of.sulfonic.acid, data$smoking_status, mean)
+(moyennes_par_etat = tapply(data$Albumin.adduct.of.sulfonic.acid, data$smoking_status, mean))
 barplot(height = moyennes_par_etat, col = "skyblue", main = "Sulfonic" )
 
-moyennes_par_etat = tapply(data$Albumin.unadducted, data$smoking_status, mean)
+(moyennes_par_etat = tapply(data$Albumin.unadducted, data$smoking_status, mean))
 barplot(height = moyennes_par_etat, col = "skyblue", main = "Non lié" )
 
 kruskal.test(data$Albumin.adduct.of.Nacetylcysteine, data$smoking_status)
@@ -243,8 +238,6 @@ type_Albumine = c(albu_CysGly$type_albumine, albu_Nacetyl$type_albumine, albu_su
 data_corr_graph = data.frame(val_albu, type_Albumine)
 data_corr_graph
 
-
-
 library(ggplot2)
 # boxplot général pour voir que c'est mieux
 ggplot(data_corr_graph, aes(x=type_Albumine,y=val_albu, fill=type_Albumine)) +
@@ -263,7 +256,7 @@ ggplot(data_corr_graph, aes(x=type_Albumine,y=val_albu, fill=type_Albumine)) +
   theme(axis.text.x = element_text(angle=30, hjust=1, vjust=1))+
   theme(legend.position="none")+
   ggtitle("Boxplot des différentes albumines")+
-  ylim(c(0,1))
+  ylim(c(0,5))
 
 
 
@@ -274,25 +267,181 @@ data$CysGly_cor = albu_CysGly$albumine
 data$Nacetyl_corr = albu_Nacetyl$albumine
 data$sulfo_corr = albu_sulfo$albumine
 data$unadducted_corr = albu_non_undu$albumine
+
+
+# Corrélation / ACP
+
+
+(X = data[12:15])
+(X2 = data[8:11])
+(Y = data[, 7])
+
+
+PCA = prcomp(na.omit(X))
+PCA2 = prcomp(X2)
+out <- summary(PCA2)
+ev <- out$importance[2, ]
+cum_ev <- out$importance[3, ]
+
+plot(cum_ev,
+     pch = 19, col = "navy", las = 1, type = "b", ylim = c(0, 1),
+     ylab = "Cumulative proportion of explained variance", xlab = "PCs", cex.lab = 1.5
+)
+
+
+
+(mypal <- brewer.pal(n = length(unique(Y)), name = "Paired"))
+(mycolors <- mypal[as.numeric(as.factor(Y))])
+
+plot(PCA2$x[, 1:2],
+     pch = 19, las = 1, col = mycolors, cex.lab = 1.5,
+     xlab = substitute(PC[1] * " (" * a * "% e.v.)", list(a = round(ev[1] * 100, digits = 2))),
+     ylab = substitute(PC[2] * " (" * a * "% e.v.)", list(a = round(ev[2] * 100, digits = 2)))
+)
+legend("topright", pch = 19, col = mypal, legend = levels(as.factor(Y)), pt.cex = 1, cex = 1.2)
+
+
+par(mfrow = c(1, 1))
+mycor <- cor(na.omit(X), PCA$x[, 1:2])
+plot(mycor[, 1:2],
+     xlim = c(-1.5, 1.5), ylim = c(-1.5, 1.5), cex = 0.1, pch = 19, las = 1, cex.lab = 1.5,
+     xlab = substitute(PC[1] * " (" * a * "% e.v.)", list(a = round(ev[1] * 100, digits = 2))),
+     ylab = substitute(PC[2] * " (" * a * "% e.v.)", list(a = round(ev[2] * 100, digits = 2)))
+)
+arrows(
+  x0 = rep(0, nrow(mycor)), y0 = rep(0, nrow(mycor)),
+  x1 = mycor[, 1], y1 = mycor[, 2], length = 0.1, col = "navy"
+)
+abline(h = 0, lty = 2)
+abline(v = 0, lty = 2)
+xseq <- seq(-1, 1, length.out = 10000)
+lines(xseq, sqrt(1 - xseq^2))
+lines(xseq, -sqrt(1 - xseq^2))
+text(mycor[, 1] + sign(mycor[, 1]) * 0.25, mycor[, 2] + 0.1, labels = colnames(X), cex = 1.2, col = "navy")
+
+
+# Coorélation test
 pairs(data.frame(data$CysGly_cor, data$unadducted_corr, data$sulfo_corr, data$Nacetyl_corr))
+plot(data$CysGly_cor, data$unadducted_corr)
+
+help(cor.test) #spearman cra ne suit pas loi normale
+cor.test(data$Nacetyl_corr, data$CysGly_cor, method = "spearman" ) # p value =  3.979e-11
+cor.test(data$Albumin.adduct.of.Nacetylcysteine, data$Albumin.adduct.of.CysGly, method = "spearman")
+
+cor.test(data$Nacetyl_corr, data$sulfo_corr, method = "spearman")# p = 6.324e-07
+cor.test(data$Albumin.adduct.of.Nacetylcysteine, data$Albumin.adduct.of.sulfonic.acid, method = "spearman")
+
+cor.test(data$Nacetyl_corr, data$unadducted_corr, method = "spearman") #p = 0.02825
+cor.test(data$Albumin.adduct.of.Nacetylcysteine, data$Albumin.unadducted, method = "spearman")
+
+
+cor.test(data$CysGly_cor, data$sulfo_corr, method = "spearman") # 0.097
+cor.test(data$Albumin.adduct.of.CysGly, data$Albumin.adduct.of.sulfonic.acid, method = "spearman") # 0.047
+
+cor.test(data$CysGly_cor, data$unadducted_corr, method = "spearman") # 2.236e-09
+cor.test(data$Albumin.adduct.of.CysGly, data$Albumin.unadducted, method = "spearman")
+
+
+cor.test(data$unadducted_corr, data$sulfo_corr, method = "spearman")# 0.1271
+cor.test(data$Albumin.unadducted, data$Albumin.adduct.of.sulfonic.acid, method = "spearman") #0.006
 
 
 
+for (i in seq(12,15)){
+  mymodel = lm(c(data[,i])~data$Nacetyl_corr)
+  qqnorm(mymodel$residuals)
+}
+
+for (i in seq(12,15)){
+  mymodel = lm(c(data[,i])~data$sulfo_corr)
+  qqnorm(mymodel$residuals)
+}
+
+for (i in seq(12,15)){
+  mymodel = lm(c(data[,i])~data$CysGly_cor)
+  qqnorm(mymodel$residuals)
+}
+# modèle de régression linéaire non valide, car pas de normalité des résidus
 
 
 
+# test de dépendance
+
+chisq.test(table(data$case, data$smoking_status)) # p value = 3.058e-7 donc statut tabgique influe sur cancer
+barplot(table(data$case, data$smoking_status), legend.text = c("Sain","Cancer"))
+####### A refaire sur ggplot
+mymodel2 = glm(data$case ~ data$smoking_status, family = "binomial")
+summary(mymodel2)
+# baisse logit de la proba de 1.12 si former et de 2.06 si never comparé à Current
+
+chisq.test(table(data$centre, data$case)) # Pas de diff entre les centres pour cancer et tabgisme
+chisq.test(table(data$centre, data$smoking_status))
+
+# Test moyenne entre adductomic et cancer
+
+mean(na.omit(data$CysGly_cor[data$smoking_status == "Former"]),)
+mean(na.omit(data$CysGly_cor[data$smoking_status == "Current"]),)
+mean(na.omit(data$CysGly_cor[data$smoking_status == "Never"]),)
 
 
+mean(na.omit(data$sulfo_corr[data$smoking_status == "Former"]),)
+mean(na.omit(data$sulfo_corr[data$smoking_status == "Current"]),)
+mean(na.omit(data$sulfo_corr[data$smoking_status == "Never"]),)
+
+mean(na.omit(data$Nacetyl_corr[data$smoking_status == "Former"]),)
+mean(na.omit(data$Nacetyl_corr[data$smoking_status == "Current"]),)
+mean(na.omit(data$Nacetyl_corr[data$smoking_status == "Never"]),)
+
+mean(na.omit(data$unadducted_corr[data$smoking_status == "Former"]),)
+mean(na.omit(data$unadducted_corr[data$smoking_status == "Current"]),)
+mean(na.omit(data$unadducted_corr[data$smoking_status == "Never"]),)
+
+wilcox.test(data$CysGly_cor[data$case == "0"], data$CysGly_cor[data$case == "1"])
+summary(glm(data$case ~ data$CysGly_cor, family = "binomial"))
+wilcox.test(data$Albumin.adduct.of.CysGly[data$case == "0"], data$Albumin.adduct.of.CysGly[data$case == "1"])
+summary(glm(data$case ~ data$Albumin.adduct.of.CysGly, family = "binomial"))
+
+wilcox.test(data$Nacetyl_corr[data$case == "0"], data$Nacetyl_corr[data$case == "1"])
+summary(glm(data$case ~ data$Nacetyl_corr, family = "binomial"))
+wilcox.test(data$Albumin.adduct.of.Nacetylcysteine[data$case == "0"], data$Albumin.adduct.of.Nacetylcysteine[data$case == "1"])
+summary(glm(data$case ~ data$Albumin.adduct.of.Nacetylcysteine, family = "binomial"))
+
+wilcox.test(data$Albumin.unadducted[data$case == "0"], data$Albumin.unadducted[data$case == "1"])
+summary(glm(data$case ~ data$Albumin.unadducted, family = "binomial"))
+wilcox.test(data$unadducted_corr[data$case == "0"], data$unadducted_corr[data$case == "1"])
+summary(glm(data$case ~ data$unadducted_corr, family = "binomial"))
+
+wilcox.test(data$sulfo_corr[data$case == "0"], data$sulfo_corr[data$case == "1"])
+summary(glm(data$case ~ data$sulfo_corr, family = "binomial"))
+wilcox.test(data$Albumin.adduct.of.sulfonic.acid[data$case == "0"], data$Albumin.adduct.of.sulfonic.acid[data$case == "1"])
+summary(glm(data$case ~ data$Albumin.adduct.of.sulfonic.acid, family = "binomial"))
 
 
+kruskal.test(data$CysGly_cor, data$smoking_status)
+kruskal.test(data$Albumin.adduct.of.CysGly, data$smoking_status)
+
+kruskal.test(data$Nacetyl_corr, data$smoking_status)
+kruskal.test(data$Albumin.adduct.of.Nacetylcysteine, data$smoking_status)
+dunn.test(data$Nacetyl_corr, data$smoking_status,  method = "bonferroni")
+dunn.test(data$Albumin.adduct.of.Nacetylcysteine, data$smoking_status,  method = "bonferroni")
+
+kruskal.test(data$Albumin.unadducted, data$smoking_status)
+dunn.test(data$Albumin.unadducted, data$smoking_status,  method = "bonferroni")
+kruskal.test(data$unadducted_corr, data$smoking_status)
+dunn.test(data$unadducted_corr, data$smoking_status,  method = "bonferroni")
 
 
+kruskal.test(data$Albumin.adduct.of.sulfonic.acid, data$smoking_status)
+kruskal.test(data$sulfo_corr, data$smoking_status)
 
 
-
-
-
-
+help(tapply)
+# Ecart type
+ecart_types_par_etat <- tapply(data$CysGly_cor, data$smoking_status, sd)
+# barplot 
+bp = barplot(height = moyennes_par_etat, col = "skyblue", main = "CysGly", ylim = c(0,5) )
+# Barre d'erreur sd
+arrows(bp, moyennes_par_etat, bp, moyennes_par_etat + ecart_types_par_etat, angle = 90, code = 3, length = 0.1, col = "black")
 
 
 
